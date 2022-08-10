9a1b4e48768bda24b71670c7e87a165c5ab5277c
Spiros Katsamakas <spiroskatsamakas@gmail.com>
2022-08-10 14:30:11 +0300

    Added changes htmls.

--- theblog/views.py
+++ theblog/views.py
@@ -15,7 +15,11 @@ class HomeView(ListView):
     ordering = ['-post_date']
     #ordering = ['-id']
     
-    
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        context["cat_menu"] = cat_menu
+        return context
 
 def CategoryView(request, cats):
 	category_posts = Post.objects.filter(category=cats.replace('-', ' '))
