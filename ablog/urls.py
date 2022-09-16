cf7f263b01bdb7d043b9dd83cff73f8e9b1e267c
Spiros Katsamakas <spiroskatsamakas@gmail.com>
2022-09-16 17:44:17 +0300

    Modified templates to display posts from database.

--- ablog/urls.py
+++ ablog/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-#from django.conf import settings
-#from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -25,4 +25,5 @@ urlpatterns = [
     path('', include('theblog.urls')),
     path('members/', include('django.contrib.auth.urls')),
     path('members/', include('members.urls')),
-]
+    
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- theblog/forms.py
+++ theblog/forms.py
@@ -30,7 +30,7 @@ for item in choices:
 class PostForm(forms.ModelForm):
     class Meta:
             model = Post
-            fields = ('title', 'title_tag', 'author', 'category', 'region', 'cultivation', 'body','snippet',)     
+            fields = ('title', 'title_tag', 'author', 'category', 'region', 'cultivation', 'body','snippet', 'header_image')     
             widgets = {
                 'title': forms.TextInput(attrs={'class': 'form-control'}),
                 'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
