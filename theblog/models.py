cd87d893ee79bb685b653d0f5df9f63afaa9d706
Spiros Katsamakas <spiroskatsamakas@gmail.com>
2022-09-16 16:24:49 +0300

    Outinh ckeditor.

--- theblog/models.py
+++ theblog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from datetime import datetime, date
-from ckeditor.fields import RichTextField
+#from ckeditor.fields import RichTextField
 
 # Create your models here.
 
@@ -41,8 +41,8 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     title_tag = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    #body = models.TextField()
-    body = RichTextField(blank=True, null=True)
+    body = models.TextField(default='text1')
+    #body = RichTextField(blank=True, null=True)
     post_date = models.DateField(auto_now_add=True)
     category = models.CharField(max_length=255, default='coding')
     region = models.CharField(max_length=255, default='thessaly')
