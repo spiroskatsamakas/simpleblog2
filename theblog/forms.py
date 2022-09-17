27a9130424a9140c1a80cabb8dbb7a8d104ec322
Spiros Katsamakas <spiroskatsamakas@gmail.com>
2022-09-17 15:05:31 +0300

    Modified no image .

--- theblog/forms.py
+++ theblog/forms.py
@@ -30,7 +30,7 @@ for item in choices:
 class PostForm(forms.ModelForm):
     class Meta:
             model = Post
-            fields = ('title', 'title_tag', 'author', 'category', 'region', 'cultivation', 'body','snippet', 'header_image')     
+            fields = ('title', 'title_tag', 'author', 'category', 'region', 'cultivation', 'body','snippet')     
             widgets = {
                 'title': forms.TextInput(attrs={'class': 'form-control'}),
                 'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
